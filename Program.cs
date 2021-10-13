810f94cd1bd3ba21d8819ec85d2e0fd2b69791ca
BogunPathmaraj <bpathmar@my.centennialcollege.ca>
2021-10-12 20:40:06 -0400

    SpriteCollision

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BogunP_300829172_A1
+{
+    class Sprites
+    {
+
+        static void Main(string[] args)
+        {
+            var name = "S3";
+            var diameter = 2;
+            var xcoordinate = 3;
+            var ycoordinate = 2;
+
+
+
+
+            Sprite s1 = new Sprite("S1", 1);
+            Sprite s2 = new Sprite("S2", 1, 1, 1);
+            Sprite s3 = new Sprite(name, diameter, xcoordinate, ycoordinate);
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+            Console.WriteLine(s3);
+            s1.Move(Direction.Right);
+            s1.Move(Direction.Right);
+            s1.Move(Direction.Up);
+            s1.Move(Direction.Left);
+            s1.Move(Direction.Up);
+            s1.Move(Direction.Left);
+            s1.Move(Direction.Up);
+            s1.Move(Direction.Right);
+            s1.Move(Direction.Up);
+            s1.Move(Direction.Right);
+            s1.Move(Direction.Right);
+
+            s2.Move(Direction.Left);
+
+            s1.Move(Direction.Down);
+            s1.Move(Direction.Down);
+
+
+        }
+    }
+}
---
+++ sprite_300829172.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BogunP_300829172_A1
+{
+    class Sprite
+    {
+        public readonly double Diameter;
+
+        private static List<Sprite> allSprites = new List<Sprite>();
+
+        public string Name;
+        public double XCoordinate { get; private set; }
+        public double YCoordinate { get; private set; }
+
+        public Sprite(string name, double diameter, int x = 0, int y = 0)
+        {
+            Name = name;
+            Diameter = diameter;
+            XCoordinate = x;
+            YCoordinate = y;
+            allSprites.Add(this);
+        }
+
+        private bool isCollision(Sprite encounter)
+        {
+            var dia = (this.Diameter + encounter.Diameter) / 2;
+            var dist = Math.Sqrt(((this.XCoordinate - encounter.XCoordinate) * (this.XCoordinate - encounter.XCoordinate)) + ((this.YCoordinate - encounter.YCoordinate) * (this.YCoordinate - encounter.YCoordinate)));
+            return dist < (dia);
+
+        }
+
+        private bool anyColison()
+        {
+
+            bool flag = false;
+            foreach (Sprite sprite in allSprites)
+            {
+                //Making sure we are not comparing current sprite to the sprite in the list as this would be a false collision
+                if (this.Name != sprite.Name && this.isCollision(sprite))
+                {
+
+                    return true;
+                }
+                else
+                {
+                    flag = false;
+                }
+            }
+            return flag;
+        }
+
+        public void Move(Direction direction)
+        {
+
+
+            Console.WriteLine($"Moving {Name}:({XCoordinate},{YCoordinate},{Diameter}) {direction}");
+
+
+            switch (direction)
+            {
+                case Direction.Up:
+                    {
+                        YCoordinate++;
+                        break;
+                    }
+                case Direction.Down:
+                    {
+                        YCoordinate--;
+
+                        break;
+                    }
+                case Direction.Right:
+                    {
+                        XCoordinate++;
+
+                        break;
+                    }
+                case Direction.Left:
+                    {
+                        XCoordinate--;
+
+                        break;
+                    }
+            }
+
+            //If collision occurs then have the sprite return to its orignal position
+            if (anyColison())
+            {
+
+                switch (direction)
+                {
+                    case Direction.Up:
+                        {
+                            YCoordinate--;
+                            Console.WriteLine($"Move {direction} failed! Collision. Position {Name}:({XCoordinate},{YCoordinate},{Diameter})");
+                            break;
+                        }
+                    case Direction.Down:
+                        {
+                            YCoordinate++;
+                            Console.WriteLine($"Move {direction} failed! Collision. Position {Name}:({XCoordinate},{YCoordinate},{Diameter})");
+                            break;
+                        }
+                    case Direction.Right:
+                        {
+                            XCoordinate--;
+                            Console.WriteLine($"Move {direction} failed! Collision. Position {Name}:({XCoordinate},{YCoordinate},{Diameter})");
+                            break;
+                        }
+                    case Direction.Left:
+                        {
+                            XCoordinate++;
+                            Console.WriteLine($"Move {direction} failed! Collision. Position {Name}:({XCoordinate},{YCoordinate},{Diameter})");
+                            break;
+                        }
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Move {direction} successful. Position {Name}:({XCoordinate},{YCoordinate},{Diameter})");
+            }
+
+
+
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}:({XCoordinate},{YCoordinate},{Diameter})";
+        }
+    }
+}
